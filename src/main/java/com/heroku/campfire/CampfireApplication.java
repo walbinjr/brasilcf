c0681f80cf9da6499f326061472f75e993455d6d
Walter Jr <walbinjr@gmail.com>
2018-10-30 11:52:44 -0300

    First commit

---
+++ src/main/java/com/heroku/campfire/CampfireApplication.java
@@ -0,0 +1,12 @@
+package com.heroku.campfire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampfireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampfireApplication.class, args);
+	}
+}
